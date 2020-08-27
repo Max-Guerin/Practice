0f610d5ecae9e5dbc95b6e2890d6a3d0c8681bd8
Max-Guerin <guerin_max@hotmail.com>
2020-08-27 14:25:37 +1000

    copied old html and css into new folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,20 +5,63 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="main-app">
+     <table cellspacing="10">
+                <tr>
+                    <td><img className="main-img" src="./images/coast.jpg" alt="Coast" height="500px" width="1000px" /></td>
+                    <td><h1>Max Guerin</h1>
+                    <p><em>Junior Web Developer.</em></p>
+                    <p>Learning how to create a website using HTML to start with. Learn CSS and Javascript further down the track.</p></td>
+                </tr>
+            </table>
+            
+            <hr size="3" noshade />
+            <h3>Things I've learned</h3>
+            <ul>
+                <li>How to start a new page.</li>
+                <li>Using header tags, paragraph tages, and lists.</li>
+                <li>Tables and Links.
+                </li>
+            </ul>
+            <hr size="3" noshade />
+            <h3>Work Experience</h3>
+            <table cellspacing="10">
+                <thead>
+                    <tr>
+                        <th>Dates</th>
+                        <th>Work</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2008-2012</td>
+                        <td>Snowboard Instructor</td>
+                    </tr>
+                    <tr>
+                        <td>2010-2018</td>
+                        <td>Trail builder</td>
+                    </tr>
+                </tbody>               
+            </table>
+            <hr size="3" noshade />
+            <h3> Skills </h3>
+            <table cellspacing="10"> 
+                <tr>
+                    <td>Snowboarding</td>
+                    <td>⭐⭐⭐⭐⭐</td>                                           
+                    <td>Surfing</td>
+                    <td>⭐⭐⭐</td>   
+                </tr>                        
+                <tr>                                                           
+                    <td>Mountain Biking</td> 
+                    <td>⭐⭐⭐⭐</td> 
+                    <td>Web Development</td>
+                    <td>⭐⭐</td>                           
+                </tr>                    
+            </table>
+       
+       </div> 
+
     </div>
   );
 }
